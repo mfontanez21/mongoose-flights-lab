9f27a64c5833266c35e44b95d9e43b290d1a084f
melvinf21 <melvinfontanez@gmail.com>
2023-04-14 19:04:36 -0400

    renders NEW FLIGHTS view

---
+++ controllers/flights.js
@@ -0,0 +1,30 @@
+import { Flight } from '../models/flight.js'
+
+function newFlight(req, res){
+  res.render('flights/new', {
+    title: 'Add Flight',
+  })
+}
+
+function create(req, res){
+  Flight.create(req.body)
+  .then(flight =>{
+    res.redirect('flights/new')
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/flights/new')
+  })
+}
+
+
+
+
+
+
+
+
+export {
+  newFlight as new,
+  create,
+}
---
+++ routes/flights.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import * as flightsCtrl from '../controllers/flights.js'
+
+const router = Router()
+
+
+// GET /flights/new
+router.get('/new', flightsCtrl.new)
+
+//POST /flights
+router.post('/', flightsCtrl.create)
+
+export {
+	router
+}
